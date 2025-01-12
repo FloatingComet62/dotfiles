71d4fdba1beda31a96e5bf4c06ec5946efec9eaf
FloatingComet62 <shubh2007rai@gmail.com>
2025-01-12 14:21:26 +0530

    message

--- update.sh
+++ update.sh
@@ -1,16 +1,17 @@
 echo $1
 
-if [[ $1 -eq "load_in" ]] then
+if [[ "$1" == "load_in" ]]
+then
   echo "load_in"
   sudo cp /etc/nixos/* ~/dotfiles/nixos/ -r
   sudo cp ~/.config/nvim/* ~/dotfiles/nvim/ -r
 fi
-if [[ $1 -eq "load_out" ]] then
+if [[ "$1" == "load_out" ]] then
   echo "load_out"
   sudo cp  ~/dotfiles/nixos/* /etc/nixos/ -r
   sudo cp  ~/dotfiles/nvim/* ~/.config/nvim/ -r
 fi
-if [[ $1 -eq "update" ]] then
+if [[ "$1" == "update" ]] then
   echo "update"
   message = "update"
   message += date +%Y/%m/%d-%H:%M:%S
