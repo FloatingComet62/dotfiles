01f3965de5d8deb63a61a447b1a57b3f78764435
FloatingComet62 <shubh2007rai@gmail.com>
2025-01-12 15:33:39 +0530

    update 2025/01/12-15:33:39

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,6 +1,7 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# Nix Channels
+# nix-ld https://github.com/Mic92/nix-ld/archive/main.tar.gz
+# nixos https://channels.nixos.org/nixos-24.11
+# nixos-unstable https://nixos.org/channels/nixos-unstable
 
 { config, pkgs, ... }:
 let
@@ -10,12 +11,9 @@ let
     };
   };
 in {
-  imports =
-    [ # Include the results of the hardware scan.
+  imports = [
       ./hardware-configuration.nix
-    ];
-
-  # Bootloader.
+  ];
   boot.loader = {
     grub = {
       enable = true;
@@ -55,7 +53,6 @@ in {
     systemd-boot.enable = false;
   };
 
-  # https://fosstodon.org/@lhf/110661879816118061
   system.activationScripts.diff = {
     supportsDryActivation = true;
     text = ''
@@ -63,22 +60,11 @@ in {
     '';
   };
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Asia/Kolkata";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_IN";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_IN";
     LC_IDENTIFICATION = "en_IN";
@@ -95,10 +81,7 @@ in {
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -106,18 +89,8 @@ in {
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.aargh = {
     isNormalUser = true;
     description = "aargh";
@@ -125,21 +98,15 @@ in {
     packages = with pkgs; [
       firefox
       kate
-    #  thunderbird
     ];
   };
-  environment.variables = {
-    HOME = "/home/aargh";
-  };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = "nix-command flakes";
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # $ nix search <package>
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     wget
     git
     neofetch
@@ -163,47 +130,16 @@ in {
   ];
 
   time.hardwareClockInLocalTime = true;  
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-  programs.tmux = {
-    enable = true;
-    clock24 = true;
-    extraConfig = ''
-'';
-  };
   programs.zsh.enable = true;
   programs.nix-ld = {
     enable = true;
     libraries = with pkgs; [
       stdenv.cc.cc.lib
-      zlib # numpy
+      zlib
     ];
   };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.flatpak.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "24.11";
 
 }
