92a948a010cf4e031533cdf05eff3bad4cb82333
FloatingComet62 <shubh2007rai@gmail.com>
2025-01-12 16:44:12 +0530

    update 2025/01/12-16:44:12

--- update.sh
+++ update.sh
@@ -3,13 +3,16 @@ then
   echo "load_in"
   sudo cp /etc/nixos/* ~/dotfiles/nixos/ -r
   sudo cp ~/.config/nvim/* ~/dotfiles/nvim/ -r
+  sudo cp ~/.zshrc ~/dotfiles/
+  sudo cp ~/.gitconfig ~/dotfiles/
 fi
 if [[ "$1" == "load_out" ]] then
   echo "load_out"
   sudo cp  ~/dotfiles/nixos/* /etc/nixos/ -r
   sudo cp  ~/dotfiles/nvim/* ~/.config/nvim/ -r
+  sudo cp ~/dotfiles/.gitconfig ~/
 fi
-if [[ "$1" == "update" ]] then
+if [[ "$1" == "git" ]] then
   echo "update"
   message="$(date +%Y/%m/%d-%H:%M:%S)"
   git add .
