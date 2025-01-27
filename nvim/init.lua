92ff29200385789881f377468b2c4576efc85044
FloatingComet62 <shubh2007rai@gmail.com>
2025-01-27 10:00:44 +0530

    update 2025/01/27-10:00:44

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,14 @@ vim.g.mapleader = " "
 vim.opt.number = true
 vim.opt.wrap = false
 
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 0
+
+-- Use spaces instead of tabs
+vim.opt.expandtab = true
+
 -- Split open explorer
 vim.keymap.set('n', '<leader><A-e>', ':Ex<CR>', { noremap = true, silent = true }) -- Replace Current
 vim.keymap.set('n', '<leader><S-e>', ':Sex<CR>', { noremap = true, silent = true }) -- Horizontal
@@ -23,3 +31,9 @@ vim.keymap.set('n', '<leader>h', ':hide<CR>', { noremap = true, silent = true })
 -- Switch buffer
 vim.keymap.set('n', '<leader>n', ':bnext<CR>', { noremap = true, silent = true })     -- Next Buffer
 vim.keymap.set('n', '<leader>p', ':bprevious<CR>', { noremap = true, silent = true }) -- Previous Buffer
+
+-- Navigate between buffers
+vim.keymap.set('n', '<A-k>', ':wincmd k<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<A-j>', ':wincmd j<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<A-h>', ':wincmd h<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<A-l>', ':wincmd l<CR>', { noremap = true, silent = true })
