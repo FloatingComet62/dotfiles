ac3eb896d1a34ab83f96079cbc6d3938865826de
Aargh Rai <shubh2007rai@gmail.com>
2025-10-06 01:12:35 +0530

    gruvbox terminal + foot + home-manager

--- sync.sh
+++ sync.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-sudo rm -rf hypr nixos nvim tmux waybar wofi yazi starship.toml
+sudo rm -rf hypr foot nixos nvim tmux waybar wofi yazi home-manager starship.toml
 sudo cp -r ~/.config/hypr .
+sudo cp -r ~/.config/foot .
 sudo cp -r /etc/nixos .
 sudo cp -r ~/.config/nvim .
 sudo cp -r ~/.config/tmux .
 sudo cp -r ~/.config/waybar .
 sudo cp -r ~/.config/wofi .
 sudo cp -r ~/.config/yazi .
+sudo cp -r ~/.config/home-manager .
 sudo cp -r ~/.config/starship.toml .
