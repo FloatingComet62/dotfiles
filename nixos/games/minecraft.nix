54b39c5aa8ad52c341b8ac88c73fab194ffae9e9
Aargh Rai <shubh2007rai@gmail.com>
2026-05-17 15:32:59 +0530

    prismlauncher nvidia

--- nixos/games/minecraft.nix
+++ nixos/games/minecraft.nix
@@ -1,8 +1,16 @@
-{ config, pkgs, ... }:
+{ config, pkgs, opt, ... }:
 {
   environment.systemPackages = with pkgs; [
     prismlauncher
-  ];
+  ] ++ (if opt.nvidia then [
+    (writeShellScriptBin "prismlauncher-nvidia" ''
+      exec env \
+        __NV_PRIME_RENDER_OFFLOAD=1 \
+        __GLX_VENDOR_LIBRARY_NAME=nvidia \
+        __VK_LAYER_NV_optimus=NVIDIA_only \
+        ${pkgs.prismlauncher}/bin/prismlauncher "$@"
+    '')
+  ] else []);
 
   # you do need to enable all of the ports because minecraft LAN uses a fucking
   # random one
