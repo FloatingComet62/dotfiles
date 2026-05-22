c40a12b159464acc1de4c6d8a0a91b1881a5f510
Aargh Rai <shubh2007rai@gmail.com>
2026-05-22 17:29:15 +0530

    pgrep isn't working

--- hypr/random-wall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-while true; do
-  wall=$(find "$HOME/Pictures/Wallpapers" -type f | shuf -n 1)
-  swaybg -i "$wall" -m fill &
-  pid=$!
-  sleep 600
-  kill $pid
-done
---
+++ hypr/wall.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+swaybg -i ~/Pictures/nurberg/wallpaperbetter.jpg -m fill
+
+# while true; do
+#   wall=$(find "$HOME/Pictures/Wallpapers" -type f | shuf -n 1)
+#   swaybg -i "$wall" -m fill &
+#   pid=$!
+#   sleep 600
+#   kill $pid
+# done
